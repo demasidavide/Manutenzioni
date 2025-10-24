0a3b96804e03a419baaa1bc89ad6ddeac9edc353
Davide De Masi <demasi.dav@gmail.com>
2025-10-24 17:44:58 +0200

    gestione registrazione con controllo inserimento in js e ajax

--- assets/index/functions_index.js
+++ assets/index/functions_index.js
@@ -10,7 +10,7 @@ document.getElementById("form_register").style.display='none';
 // funzione per nascondere div risultato inserimento
 function showresult(){
     document.getElementById("result").style.display="block";
-    setTimeout(function() {
+    setTimeout(()=> {
   document.getElementById('result').style.display = "none";
 }, 5000);
 }
@@ -19,15 +19,23 @@ function showresult(){
 document.getElementById('register_form').onsubmit = function(e) {
   e.preventDefault();
   const formData = new FormData(this);
-  fetch('register.php', {
+  fetch('assets/index/register.php', {
     method: 'POST',
     body: formData
   })
   .then(r => r.text())
   .then(data => {
     document.getElementById('result').innerText = data;
+    //controllo testo risposta per settare classi diverse
+    if(data.includes("Registrazione effettuata")){
+      document.getElementById('result').className= "result-ok";
+    }else{
+      document.getElementById('result').className="result-no";
+    }
     showresult();
+    setTimeout(()=>{
+      document.getElementById("register_form").style.display='none';
+    },5000);
     document.getElementById("register_form").reset();
-    closemodal();
   });
 };
--- assets/index/register.php
+++ assets/index/register.php
@@ -7,13 +7,13 @@ $user=$_POST["username"];
 $psw=$_POST["psw"];
 
 // preparo la query per inserimento dati con stmt
+
 $stmt=$conn->prepare("INSERT INTO utente (nome,cognome,username,psw) VALUES (?, ?, ?, ?)");
 $stmt->bind_param("ssss", $nome, $cognome,$user,$psw);
 $stmt->execute();
 
  if ($stmt->affected_rows>0){
-    echo"Utente inserito con successo";
-    
+    echo"Registrazione effettuata";
  }else{
     echo"Errore: Utente non inserito";
  }
